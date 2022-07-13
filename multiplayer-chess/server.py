b2b746c9d1ca64d64b82e1282998f4d23e8896e4
hanchung <ghoo1125@gmail.com>
2022-07-13 15:01:42 +0800

    Python socket example

---
+++ multiplayer-chess/server.py
@@ -0,0 +1,63 @@
+import socket
+from _thread import *
+
+server = "localhost"
+port = 5555
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    s.bind((server, port))
+except socket.error as e:
+    str(e)
+
+
+s.listen(2)
+print("Waiting for connection, Server Started")
+
+
+def read_pos(pos):
+    pos = pos.split(",")
+    return int(pos[0]), int(pos[1])
+
+
+def make_pos(tup):
+    return str(tup[0]) + "," + str(tup[1])
+
+
+pos = [(0, 0), (100, 100)]
+
+
+def threaded_client(conn, player):
+    conn.send(str.encode(make_pos(pos[player])))
+    reply = ""
+    while True:
+        try:
+            data = read_pos(conn.recv(2048).decode())
+            pos[player] = data
+
+            if not data:
+                print("Disconnected")
+                break
+            else:
+                if player == 1:
+                    reply = pos[0]
+                else:
+                    reply = pos[1]
+                print("Recieved:", data)
+                print("Sending:", reply)
+            conn.sendall(str.encode(make_pos(reply)))
+        except:
+            break
+
+    print("Lost connection")
+    conn.close()
+
+
+currentPlayer = 0
+while True:
+    conn, addr = s.accept()
+    print("Connected to:", addr)
+
+    start_new_thread(threaded_client, (conn, currentPlayer))
+    currentPlayer += 1
